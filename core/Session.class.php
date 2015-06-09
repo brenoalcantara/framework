852fa0e437453ddf72e88f7a22166de6af7db469
Breno <brenosalcantara@hotmail.com>
2015-06-09 01:39:31 -0300

    Primeiro commit

---
+++ core/Session.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Core
+ * 
+ * Classe final para gerenciamento da sessão
+ *
+ * @access public
+ * @author Breno Alcantara <contato.breno@gmail.com>
+ * @copyright 2015 Breno Alcantara
+ * @license MIT
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @package Core
+ * @version 1.0.0
+ */
+
+namespace Core;
