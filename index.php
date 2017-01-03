940adfebcfd9f19f2e9fef703f384af4d327043d
Breno <breno@localhost.localdomain>
2017-01-03 17:25:37 -0200

    Implementado autoload com spl.

--- index.php
+++ index.php
@@ -3,12 +3,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+//spl_autoload_register(function ($class) {
+//    require_once(str_replace('src\\', '/', $class . '.php'));
+//});
 
-spl_autoload_register(function ($class) {
-    require_once(str_replace('\\', '/', $class . '.class.php'));
-});
+function autoload ($class) {
+    include_once('src/' . str_replace('\\', '/', $class) . '.php');
+}
+spl_autoload_register('autoload');
 
 
-$teste = new Model\Example();
-
-echo $teste->__get('password');
+$teste = new \App\Model\Example();
+var_dump($teste->__get('password'));
