b7993afbf2857e1217903f4d963580ef3d3800fe
Breno <breno@localhost.localdomain>
2017-01-03 17:27:09 -0200

    Alteração no index.php

--- index.php
+++ index.php
@@ -3,10 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//spl_autoload_register(function ($class) {
-//    require_once(str_replace('src\\', '/', $class . '.php'));
-//});
-
 function autoload ($class) {
     include_once('src/' . str_replace('\\', '/', $class) . '.php');
 }
